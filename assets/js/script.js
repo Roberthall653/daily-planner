2afcfa2a8fdaf85f6da4196ff1031193c61fc41b
Roberthall653 <roberthall653@gmail.com>
2023-05-18 09:20:38 -0700

    final project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,123 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var timeBlock = $('.time-block');
+var hourBlock = $('.hour');
+var hourDigit = [""];
+var hour9 = $('#hour-9');
+var hour10 = $('#hour-10');
+var hour11 = $('#hour-11');
+var hour12 = $('#hour-12');
+var hour1 = $('#hour-1');
+var hour2 = $('#hour-2');
+var hour3 = $('#hour-3');
+var hour4 = $('#hour-4');
+var hour5 = $('#hour-5');
+var hour9array = [];
+var hour10array = [];
+var hour11array = [];
+var hour12array = [];
+var hour1array = [];
+var hour2array = [];
+var hour3array = [];
+var hour4array = [];
+var hour5array = [];
+var saveBtn = $(".saveBtn");
+var text = "";
+
+$(function formatting() {
+  saveBtn.click(function (event) {
+    event.preventDefault();
+
+    //saves the text in textarea to local storage.
+    text = $(this).prev().val();
+    if ($(this).parent().attr('id') === 'hour-9') {
+      hour9array.pop();
+      hour9array.push(text);
+      localStorage.setItem('9AM', hour9array);
+    } else if ($(this).parent().attr('id') === 'hour-10') {
+      hour10array.pop();
+      hour10array.push(text);
+      localStorage.setItem('10AM', hour10array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-11') {
+      hour11array.pop();
+      hour11array.push(text);
+      localStorage.setItem('11AM', hour11array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-12') {
+      hour12array.pop();
+      hour12array.push(text);
+      localStorage.setItem('12AM', hour12array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-1') {
+      hour1array.pop();
+      hour1array.push(text);
+      localStorage.setItem('1PM', hour1array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-2') {
+      hour2array.pop();
+      hour2array.push(text);
+      localStorage.setItem('2PM', hour2array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-3') {
+      hour3array.pop();
+      hour3array.push(text);
+      localStorage.setItem('3PM', hour3array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-4') {
+      hour4array.pop();
+      hour4array.push(text);
+      localStorage.setItem('4PM', hour4array);
+    }
+    else if ($(this).parent().attr('id') === 'hour-5') {
+      hour5array.pop();
+      hour5array.push(text);
+      localStorage.setItem('5PM', hour5array);
+    }
+  })
 });
+
+
+
+
+//used to compare dayjs time and the hour block time
+var currentTime = parseInt(dayjs().format('H'));
+hourBlock.each(function () {
+  hourDigit = ($(this).data('hour'));
+
+  if (currentTime == hourDigit) {
+    $(this).next().addClass('present');
+  }
+  else if (currentTime > hourDigit) {
+    $(this).next().addClass('past');
+  }
+  else {
+    $(this).next().addClass('future');
+  }
+});
+
+//grabs the saved text from local storage and displays it on page load
+function textDisplay() {
+  hour9.children('.description').text(
+    localStorage.getItem('9AM', hour9array));
+  hour10.children('.description').text(
+    localStorage.getItem('10AM', hour10array));
+  hour11.children('.description').text(
+    localStorage.getItem('11AM', hour11array));
+  hour12.children('.description').text(
+    localStorage.getItem('12PM', hour12array));
+  hour1.children('.description').text(
+    localStorage.getItem('1PM', hour1array));
+  hour2.children('.description').text(
+    localStorage.getItem('2PM', hour2array));
+  hour3.children('.description').text(
+    localStorage.getItem('3PM', hour3array));
+  hour4.children('.description').text(
+    localStorage.getItem('4PM', hour4array));
+  hour5.children('.description').text(
+    localStorage.getItem('5PM', hour5array));
+
+};
+//code to display the current date in the header of the page.
+var weekDay = dayjs().format('dddd');
+$('#currentDay').text(weekDay);
+
+textDisplay();
